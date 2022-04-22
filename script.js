ef3d41b9e294af3f6d51dea96e04b40bcd33f9bc
juamatx <60664640+juamatx@users.noreply.github.com>
2022-04-22 10:01:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const d = new Date();
+let day = d.getDay();
+
+if(day===0){
+    document.getElementById('lunes').innerText = 'Es Lunes'
+    document.styleSheets[0].insertRule(":root{background-color: #F97D0B;}")
+}
+else{
+    document.getElementById('lunes').innerText = 'No es Lunes'
+    document.styleSheets[0].insertRule(":root{background-color: black;}")
+    
+}
