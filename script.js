ecee87167ec5e9bb4636652677d72fd47dfa4f44
juamatx <60664640+juamatx@users.noreply.github.com>
2022-04-24 16:18:25 -0400

    day bug on sunday

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const d = new Date();
 let day = d.getDay();
 
+console.log(day)
 if(day===0){
     document.getElementById('lunes').innerText = 'Es Lunes'
     document.styleSheets[0].insertRule(":root{background-color: #F97D0B;}")
