3158373a49d2582d9f9f8c095b560fe2a9773112
juamatx <60664640+juamatx@users.noreply.github.com>
2022-04-24 16:21:17 -0400

    fixed testing bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const d = new Date();
 let day = d.getDay();
 
 console.log(day)
-if(day===0){
+if(day===1){
     document.getElementById('lunes').innerText = 'Es Lunes'
     document.styleSheets[0].insertRule(":root{background-color: #F97D0B;}")
 }
